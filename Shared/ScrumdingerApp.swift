7b5a2ecc7b8bf399c02a59579d267cb1b5a3e143
Michael M <9michaeljm@gmail.com>
2022-08-25 01:41:13 -0400

    Initial Commit

---
+++ Shared/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by Michael M on 8/25/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
