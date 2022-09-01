681167b946dbc3dc81af11c276d8b5e04d5c5f11
Michael M <9michaeljm@gmail.com>
2022-09-01 01:19:12 -0400

    meeting info section of DetailView complete

--- Shared/Models/Theme.swift
+++ Shared/Models/Theme.swift
@@ -28,11 +28,14 @@ enum Theme: String {
     
     var accentColor: Color {
         switch self {
-            case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
-            case .indigo, .magenta, .navy, .oxblood, .purple: return .white
+        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
+        case .indigo, .magenta, .navy, .oxblood, .purple: return .white
         }
     }
     var mainColor: Color {
         Color(rawValue)
+    }
+    var name: String {
+        rawValue.capitalized
     }
 }
