959491021177115345ed196ac4c3bc1772c0e41e
Michael M <9michaeljm@gmail.com>
2022-09-01 01:32:02 -0400

    "creating a navigation hierarchy" section complete

--- Shared/Models/DetailView.swift
+++ Shared/Models/DetailView.swift
@@ -13,9 +13,11 @@ struct DetailView: View {
     var body: some View {
         List {
             Section(header: Text("Meeting info")) {
-                Label("Start Meeting", systemImage: "timer")
-                    .font(.headline)
-                    .foregroundColor(.accentColor)
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
                 HStack {
                     Label("Length", systemImage: "clock")
                     Spacer()
@@ -33,7 +35,13 @@ struct DetailView: View {
                 }
                 .accessibilityElement(children: .combine)
             }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            }
         }
+        .navigationTitle(scrum.title)
     }
 }
 
